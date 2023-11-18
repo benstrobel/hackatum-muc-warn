ded2d67de7cf9761098bf567f00e99e610ab078a
Ben Strobel <benedikt.strobel@tum.de>
2023-11-18 02:19:54 +0100

    init commit

---
+++ app/src/main/java/com/muc/warn/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.muc.warn
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.p2p.WifiP2pManager
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.muc.warn.ui.theme.MucWarnTheme
+
+private var TAG = "MainActivity";
+
+class MainActivity : ComponentActivity() {
+    companion object {
+        val TAG = "MainActivity"
+        val INITIAL_PERMISSION_CHECK_ID = 1
+        val REQUIRED_PERMISSION_ARRAY = if(android.os.Build.VERSION.SDK_INT < 33) arrayOf(Manifest.permission.ACCESS_FINE_LOCATION) else arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    private lateinit var wiFiDirectReceiver: WiFiDirectBroadcastReceiver
+    private lateinit var intentFilter: IntentFilter
+    private lateinit var channel: WifiP2pManager.Channel
+    private lateinit var manager: WifiP2pManager
+
+    private var isWifiP2pEnabled = false
+
+    fun setIsWifiP2pEnabled(isWifiP2pEnabled: Boolean) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MucWarnTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Erdnuss", this)
+                }
+            }
+        }
+        manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+        channel = manager.initialize(this, mainLooper, null)
+
+        wiFiDirectReceiver = WiFiDirectBroadcastReceiver(manager, channel, this)
+        intentFilter = IntentFilter()
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.NEARBY_WIFI_DEVICES
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(REQUIRED_PERMISSION_ARRAY, INITIAL_PERMISSION_CHECK_ID)
+        }
+    }
+
+    // TODO Replace, see deprecation msg of super func
+    @Deprecated(message = "TODO Replace")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == INITIAL_PERMISSION_CHECK_ID) {
+            if(!permissions.contentEquals(REQUIRED_PERMISSION_ARRAY)) {
+                Log.e(TAG, "Closing app becuase permissions were denied")
+                this.finishAffinity() // quitting the app
+            }
+        }
+    }
+
+    // Done
+    @SuppressLint("MissingPermission")
+    fun discover() {
+        manager.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+            override fun onSuccess() {
+                Log.d(TAG, "Discovery init successfully")
+            }
+
+            override fun onFailure(reasonCode: Int) {
+                Log.e(TAG, "Discovery init failed")
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        try {
+            registerReceiver(wiFiDirectReceiver, intentFilter)
+        } catch (e: Exception) {
+            e.message?.let { Log.e(TAG, it) }
+        }
+    }
+
+    /*override fun onDestroy() {
+        super.onDestroy()
+        try {
+            unregisterReceiver(wiFiDirectReceiver)
+        } catch (e: Exception) {
+            e.message?.let { Log.e(TAG, it) }
+        }
+    }*/
+
+    override fun onPause() {
+        super.onPause()
+        try {
+            unregisterReceiver(wiFiDirectReceiver)
+        } catch (e: Exception) {
+            e.message?.let { Log.e(TAG, it) }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, activity: MainActivity?, modifier: Modifier = Modifier) {
+    Text(
+            text = "Servus $name!",
+            modifier = modifier
+    )
+    Column {
+        Button(onClick = {activity?.discover()}) {
+            Text("Start Discovery")
+        }
+        Button(onClick = { Log.d(TAG, "Pressed")}) {
+            Text("Press Button")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MucWarnTheme {
+        Greeting("Android", null)
+    }
+}
---
+++ app/src/main/java/com/muc/warn/WiFiDirectBroadcastReceiver.kt
@@ -0,0 +1,59 @@
+package com.muc.warn
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pManager
+import android.util.Log
+
+class WiFiDirectBroadcastReceiver (private val manager: WifiP2pManager, private val channel: WifiP2pManager.Channel, private val activity: MainActivity): BroadcastReceiver() {
+    companion object {
+        val TAG = "WiFiDirectBroadcastReceiver"
+    }
+
+    private val peers = mutableListOf<WifiP2pDevice>()
+    private val peerListListener = WifiP2pManager.PeerListListener { peerList ->
+        val refreshedPeers = peerList.deviceList
+        /*if (refreshedPeers != peers) {
+            peers.clear()
+            peers.addAll(refreshedPeers)
+
+            // If an AdapterView is backed by this data, notify it
+            // of the change. For instance, if you have a ListView of
+            // available peers, trigger an update.
+            (listAdapter as WiFiPeerListAdapter).notifyDataSetChanged()
+
+            // Perform any other updates needed based on the new list of
+            // peers connected to the Wi-Fi P2P network.
+        }*/
+
+        if (peers.isEmpty()) {
+            Log.d(TAG, "No devices found")
+            return@PeerListListener
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when(intent.action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                Log.d(TAG, "Received WIFI_P2P_STATE_CHANGED_ACTION Event | Value " + state)
+                when(state) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        //activity.setIsWifiP2pEnabled(true)
+                    }
+                }
+            }
+            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                Log.d(this.javaClass.name, "Received WIFI_P2P_PEERS_CHANGED_ACTION Event")
+            }
+            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+                Log.d(this.javaClass.name, "Received WIFI_P2P_CONNECTION_CHANGED_ACTION Event")
+            }
+            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                Log.d(this.javaClass.name, "Received WIFI_P2P_THIS_DEVICE_CHANGED_ACTION Event")
+            }
+        }
+    }
+}
