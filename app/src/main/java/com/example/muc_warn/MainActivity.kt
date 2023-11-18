b13057463e01450b424a0535afb34c0f166549a8
Ben Riegel <ben.r@outlook.de>
2023-11-18 20:41:12 +0100

    add intermediate translation layer

--- app/src/main/java/com/example/muc_warn/MainActivity.kt
+++ app/src/main/java/com/example/muc_warn/MainActivity.kt
@@ -3,16 +3,13 @@ package com.example.muc_warn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.muc_warn.components.BottomBar.Screen
 import com.example.muc_warn.models.NavigationViewModel
 import com.example.muc_warn.ui.theme.MucWarnTheme
 import com.example.muc_warn.views.InfoView
---
+++ app/src/main/java/com/example/muc_warn/busineslogic/PeerToPeerManager.kt
@@ -0,0 +1,43 @@
+package com.example.muc_warn.busineslogic
+
+import android.os.Handler
+import com.example.muc_warn.schema.Alert
+import com.example.muc_warn.schema.Location
+import java.util.Date
+
+interface FetchCallback{
+    fun onCallback(arr: List<Alert>)
+}
+
+class PeerToPeerManager() {
+    private val newAlerts = ArrayList<Alert>()
+    private var callback: FetchCallback? = null
+
+    init {
+        val handler = Handler()
+        val runnable = object : Runnable {
+            override fun run() {
+                newAlerts.add(
+                    Alert(
+                        senderName = "Lana Maler",
+                        title = "Starkregenwarnung",
+                        description = "Es wird erwartet, dass in den nächsten Stunden Starkregen die Region um Hauptstraße 15 in Berlin erreichen wird. Mögliche Auswirkungen sind Überschwemmungen und rutschige Straßen. Bitte nehmen Sie Vorsichtsmaßnahmen und vermeiden Sie unnötige Fahrten.",
+                        threadLevel = 2,
+                        postDate = Date(),
+                        expireDate = Date(),
+                        location = Location(52.5200, 13.4050),
+                        locationString = "Hauptstraße 15, Berlin"
+                    )
+                )
+                callback?.onCallback(newAlerts)
+                println("I simulate P2P and I add new Alert every 3 seconds")
+                handler.postDelayed(this, 3000)
+            }
+        }
+        handler.post(runnable)
+    }
+
+    fun fetchNewAlerts(callback: FetchCallback){
+        this.callback = callback
+    }
+}
